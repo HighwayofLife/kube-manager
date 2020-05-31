d6ee705f6cb5fad205bfe86fb27c466d0a806418
David Lewis <highwayoflife@gmail.com>
2020-05-30 22:44:20 -0700

    WIP: Add script options for creating kube config

--- scripts/generate_kubeconfig.sh
+++ scripts/generate_kubeconfig.sh
@@ -6,10 +6,10 @@ containerId=$( docker run --rm -t -d \
   -v $PWD:/root/kubectl \
   -v $PWD/.bashrc:/root/.bashrc \
   -v $PWD/.zshrc:/root/.zshrc \
-  kubectl-manager )
+  kube-manager )
 
 echo "Creating kube_config.yaml"
-docker exec -it $containerId bash -c "/usr/local/bin/python /root/kubectl/scripts/create_kubeconfig.py -u ${USER}; exit"
+docker exec -it $containerId bash -c "/usr/local/bin/python /var/lib/create_kubeconfig.py -u ${USER}; exit"
 
 echo "Removing container"
 docker stop $containerId
